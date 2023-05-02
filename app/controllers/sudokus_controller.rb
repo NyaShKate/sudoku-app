2905a13326a5bfdfb076bb3a56de4a8f05728380
NyaShKate <Hagster882@gmail.com>
2023-05-02 17:12:25 +0300

    changed files

--- app/controllers/sudokus_controller.rb
+++ app/controllers/sudokus_controller.rb
@@ -1,4 +1,36 @@
 class SudokusController < ApplicationController
   def index
+    # puts params
+    @sudoku = [[0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0]]
+
+    (0..8).each do |i|
+      (0..8).each do |j|
+        str = 'e'+(i*10+j).to_s
+        puts params[str.parameterize.underscore.to_sym]
+        @sudoku[i][j] = params[str.parameterize.underscore.to_sym]
+      end
+    end
+
+    if params.has_key? :check
+    #  check function
+    end
+
+    if params.has_key? :gen
+      #  auto-gen function
+    end
+
+    if params.has_key? :solve
+      #  solve function
+    end
+
+    # puts @sudoku.to_s
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  root "sudokus#index"
-  get "/sudokus", to: "sudokus#index"
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  post '/', to: "sudokus#index"
+  root "sudokus#index"
 end
